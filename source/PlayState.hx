0bb463ffbdb3c922ae8992a3dc9b117921ed75ba
Sam Bumgardner <sambumgardner1@gmail.com>
2016-09-01 12:19:39 -0500

    Added boundary hitboxes to the level and extra collision logic to ensure players can't overlap characters or push themselves out of a level.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,16 +5,21 @@ import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.addons.display.FlxBackdrop;
+import flixel.group.FlxGroup;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
+import flixel.tile.FlxTileblock;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import flixel.util.FlxCollision;
 import flixel.util.FlxSort;
 
 class PlayState extends FlxState
 {
 	private var _backdrop:FlxBackdrop;
+	private var _grpCharacters:FlxTypedGroup<FlxSprite>; //Used for player-specific collisions.
 	private var _grpActors:FlxTypedGroup<FlxSprite>;
+	private var _grpBoundaries:FlxGroup;
 	private var _mechanicChar:MechanicChar;
 	private var _robotChar:RobotChar;
 	
@@ -28,14 +33,20 @@ class PlayState extends FlxState
 		add(_backdrop);
 		_backdrop.velocity.set(0, 200);
 		
+		_grpBoundaries = FlxCollision.createCameraWall(FlxG.camera, false, 15);
+		add(_grpBoundaries);
 		
 		_grpActors = new FlxTypedGroup<FlxSprite>();
 		add(_grpActors);
 		
-		_mechanicChar = new MechanicChar(500,500);
+		_grpCharacters = new FlxTypedGroup<FlxSprite>();
+		
+		_mechanicChar = new MechanicChar(500, 500);
+		_grpCharacters.add(_mechanicChar);
 		_grpActors.add(_mechanicChar);
 		
 		_robotChar = new RobotChar(500, 500);
+		_grpCharacters.add(_robotChar);
 		_grpActors.add(_robotChar);
 		
 		
@@ -50,7 +61,14 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		
+		
+		//Have to recheck collisions once to ensure that they don't pass through each other.
+		FlxG.collide(_grpCharacters, _grpBoundaries);
 		FlxG.collide(_robotChar, _mechanicChar);
+		FlxG.collide(_grpCharacters, _grpBoundaries);
+		
+		
 		_grpActors.sort(sortByOffsetY, FlxSort.ASCENDING);
 	}
 }
