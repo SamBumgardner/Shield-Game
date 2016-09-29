43f07168f52d663ecb117612684d05df977a719a
Sam Bumgardner <sambumgardner1@gmail.com>
2016-09-29 18:54:20 -0500

    Added documentation to DamageableActor

--- source/DamageableActor.hx
+++ source/DamageableActor.hx
@@ -9,7 +9,10 @@ import flixel.system.FlxAssets.FlxGraphicAsset;
  */
 class DamageableActor extends FlxSprite
 {
-
+	///////////////////////////////////////////
+	//          DATA INITIALIZATION          //
+	///////////////////////////////////////////
+	
 	private var healthState:FSM;
 	private var vulnerable:Bool = true;
 	private var hurtTime:Int;
@@ -23,11 +26,61 @@ class DamageableActor extends FlxSprite
 		healthState = new FSM(normState);
 	}
 	
+	
+	///////////////////////////////////////////
+	//           GENERAL FUNCTIONS           //
+	///////////////////////////////////////////
+	
+	// adjustColor(colorChange:Int):Int
+	//
+	// subtracts/adds colorChange from the sprite's current tint 
+	// color using bitwise XOR. 
+	// Sets new color when called with a new parameter.
+	// Changes color back when called with the same parameter 
+	// a second time.
+	
 	private function adjustColor(colorChange:Int):Int
 	{
 		set_color(color ^ colorChange);
 		return color;
 	}
+	
+	// damaged(damage:Float):Void
+	//
+	// Called when the DamageableActor should take damage.
+	// Initiates change in the actor's healthState.
+	
+	public function damaged(damage:Float):Void
+	{
+		if (vulnerable == true)
+		{
+			hurt(damage);
+			healthState.transitionStates(hurtTransition);
+		}
+	}
+	
+	
+	///////////////////////////////////////////
+	//        HEALTHSTATE TRANSITIONS        //
+	///////////////////////////////////////////
+	
+	// Transition functions are used by a FSM to trigger switches 
+	// between states. 
+	// They have a number of responsibilities: 
+	//  
+	//  * Call any functions and change any variables that should
+	//    only be done once, when a state begins.
+	//  
+	//  * Set the current state of their FSM.
+	//  
+	//  * Return the number of frames that the state should last.
+	//    Returning -1 means the state lasts until a new transition
+	//    is called.
+	//
+	//  * If the state is on a timer, set the FSM's nextTransition
+	//    variable to whatever transtion function should be called
+	//    when the current state's timer expires.
+	
 	
 	private function normTransition():Int
 	{
@@ -35,11 +88,6 @@ class DamageableActor extends FlxSprite
 		healthState.activeState = normState;
 		vulnerable = true;
 		return -1;
-	}
-	
-	private function normState():Void
-	{
-		return;
 	}
 	
 	private function hurtTransition():Int
@@ -55,11 +103,6 @@ class DamageableActor extends FlxSprite
 		return hurtTime;
 	}
 	
-	private function hurtState():Void
-	{
-		return;
-	}
-	
 	private function recoveryTransition():Int
 	{
 		adjustColor(injuredColor);
@@ -70,6 +113,26 @@ class DamageableActor extends FlxSprite
 		return recoveryTime;
 	}
 	
+	
+	///////////////////////////////////////////
+	//          HEALTHSTATE  STATES          //
+	///////////////////////////////////////////
+	
+	// State functions are essentially extensions to their class's 
+	// update function. If a state is active, it will be called 
+	// when its FSM's update function is called.
+	
+	
+	private function normState():Void
+	{
+		return;
+	}
+	
+	private function hurtState():Void
+	{
+		return;
+	}
+	
 	private function recoveryState():Void
 	{
 		if (healthState.stateTimer % 16 < 8)
@@ -78,14 +141,10 @@ class DamageableActor extends FlxSprite
 			set_alpha(1);
 	}
 	
-	public function damaged(damage:Float):Void
-	{
-		if (vulnerable == true)
-		{
-			hurt(damage);
-			healthState.transitionStates(hurtTransition);
-		}
-	}
+	
+	///////////////////////////////////////////
+	//            UPDATE FUNCTION            //
+	///////////////////////////////////////////
 	
 	public override function update(elapsed:Float):Void
 	{
